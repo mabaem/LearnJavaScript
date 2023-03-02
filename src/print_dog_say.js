3176a4f7601a172cb1a311ba4c0b383ec713a25c
Yeji Jeong <93515120+mabaem@users.noreply.github.com>
2023-03-02 15:56:06 +0900

    Create print_dog_say.js

---
+++ src/print_dog_say.js
@@ -0,0 +1,14 @@
+const dog = {
+  name: "해피",
+  sound: "멍멍",
+  /*
+  say: function say() {
+    console.log(this.sound);
+  },
+  */
+  say: function () {
+    console.log(this.sound);
+  }
+};
+
+dog.say();
