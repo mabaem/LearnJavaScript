3a923bed4db9f61eec50ab551f992e7e054c2245
Yeji Jeong <93515120+mabaem@users.noreply.github.com>
2023-03-03 17:25:32 +0900

    Create radius.js

---
+++ src/radius.js
@@ -0,0 +1,7 @@
+// function calculateCircleArea(r = 1) {
+//   return Math.PI * r * r;
+// }
+const calculateCircleArea = (r = 1) => Math.PI * r * r;
+
+const area = calculateCircleArea();
+console.log(area);
