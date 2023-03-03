f76f5e53b7f77f43bf28652dd00ac5ae7dedb20c
Yeji Jeong <93515120+mabaem@users.noreply.github.com>
2023-03-03 10:37:50 +0900

    Create array_test.js

---
+++ src/array_test.js
@@ -0,0 +1,15 @@
+console.clear();
+const objects = [{name: '해피'}, {name: '나비'}];
+
+console.log(objects);
+console.log(objects[0]);
+console.log(objects[1]);
+console.log(objects.length);
+
+objects.push({
+  name: '바둑이'
+});
+
+console.log(objects);
+console.log(objects[2]);
+console.log(objects.length);
